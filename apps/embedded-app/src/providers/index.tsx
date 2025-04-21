5bf41857b66b05688ffa24b1de3dfcf9ab7468ca
Les Aubrey <les@wharf.digital>
2025-04-21 12:00:27 +0800

    Adds session provider scaffolding

--- apps/embedded-app/src/providers/index.tsx
+++ apps/embedded-app/src/providers/index.tsx
@@ -4,6 +4,8 @@ import "@shopify/polaris/build/esm/styles.css";
 import enTranslations from "@shopify/polaris/locales/en.json";
 import { AppProvider } from "@shopify/polaris";
 
+import { SessionProvider } from "./session-provider";
+
 type ProvidersProps = {
   children: React.ReactNode;
 };
@@ -11,5 +13,9 @@ type ProvidersProps = {
 export const Providers = (props: ProvidersProps) => {
   const { children } = props;
 
-  return <AppProvider i18n={enTranslations}>{children}</AppProvider>;
+  return (
+    <AppProvider i18n={enTranslations}>
+      <SessionProvider>{children}</SessionProvider>
+    </AppProvider>
+  );
 };
