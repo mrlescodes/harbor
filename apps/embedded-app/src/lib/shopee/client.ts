67e391a6d13e8429e36b79ad89f2fd3bae3758d4
Les Aubrey <les@wharf.digital>
2025-05-23 13:53:22 +0800

    Add api client live implementation

--- apps/embedded-app/src/lib/shopee/client.ts
+++ apps/embedded-app/src/lib/shopee/client.ts
@@ -1,10 +1,15 @@
 import { Effect, Layer } from "effect";
 
+import { ShopeeAPIClient } from "@harbor/shopee-api-client/api";
 import { ShopeeAuthClient } from "@harbor/shopee-api-client/auth";
 import { createShopeeAPIConfigLayer } from "@harbor/shopee-api-client/config";
 
 import { env } from "~/env";
 import { ShopeeTokenStorageLive } from "./token-storage";
+
+/**
+ * Config Layer
+ */
 
 const ShopeeAPIConfigLayerLive = createShopeeAPIConfigLayer({
   partnerId: env.SHOPEE_PARTNER_ID,
@@ -12,12 +17,29 @@ const ShopeeAPIConfigLayerLive = createShopeeAPIConfigLayer({
   apiBaseUrl: env.SHOPEE_API_BASE_URL,
 });
 
+/**
+ * Auth Client
+ */
+
 const ShopeeAuthClientLive = ShopeeAuthClient.Live.pipe(
   Layer.provide(ShopeeAPIConfigLayerLive),
   Layer.provide(ShopeeTokenStorageLive),
 );
 
-// Helper functions to run effects with the provided layers
 export const runWithShopeeAuthClient = <A, E>(
   effect: Effect.Effect<A, E, ShopeeAuthClient>,
 ) => Effect.provide(effect, ShopeeAuthClientLive);
+
+/**
+ * API Client
+ */
+
+const ShopeeAPIClientLive = ShopeeAPIClient.Live.pipe(
+  Layer.provide(ShopeeAPIConfigLayerLive),
+  Layer.provide(ShopeeAuthClientLive),
+);
+
+// Helper functions to run effects with the provided layers
+export const runWithShopeeAPIClient = <A, E>(
+  effect: Effect.Effect<A, E, ShopeeAPIClient>,
+) => Effect.provide(effect, ShopeeAPIClientLive);
