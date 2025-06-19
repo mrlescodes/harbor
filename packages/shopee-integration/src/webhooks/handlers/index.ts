8316b06e033455c5fd074a51a39bc92d31c2db65
Les Aubrey <les@wharf.digital>
2025-06-19 09:40:39 +0800

    Add push handler

--- packages/shopee-integration/src/webhooks/handlers/index.ts
+++ packages/shopee-integration/src/webhooks/handlers/index.ts
@@ -4,22 +4,22 @@ import type { ShopeeWebhook } from "../schema";
 import { isOrderStatusPush, validateShopeeWebhook } from "../schema";
 import { processOrderStatusPush } from "./order-status-push-handler";
 
-const processWebhook = (data: ShopeeWebhook) => {
+const processWebhook = (payload: ShopeeWebhook) => {
   return Effect.gen(function* () {
-    if (isOrderStatusPush(data)) {
-      return yield* processOrderStatusPush(data);
+    if (isOrderStatusPush(payload)) {
+      return yield* processOrderStatusPush(payload);
     }
 
     return yield* Effect.fail(
-      new Error(`Unsupported push notification code: ${data.code}`),
+      new Error(`Unsupported push notification code: ${payload.code}`),
     );
   });
 };
 
 export const shopeeWebhookHandler = (rawData: unknown) => {
   return Effect.gen(function* () {
-    const data = yield* validateShopeeWebhook(rawData);
+    const payload = yield* validateShopeeWebhook(rawData);
 
-    return yield* processWebhook(data);
+    return yield* processWebhook(payload);
   });
 };
