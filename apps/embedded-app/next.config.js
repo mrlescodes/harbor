145d8c4bf692bb679dbe47338c8dd8efa454966c
Les Aubrey <les@wharf.digital>
2025-06-19 10:23:17 +0800

    Add fix for vercel

--- apps/embedded-app/next.config.js
+++ apps/embedded-app/next.config.js
@@ -1,4 +1,5 @@
 import { fileURLToPath } from "url";
+import { PrismaPlugin } from "@prisma/nextjs-monorepo-workaround-plugin";
 import createJiti from "jiti";
 
 // Import env files to validate at build time. Use jiti so we can load .ts files in here.
@@ -12,6 +13,14 @@ const config = {
   /** We already do linting and typechecking as separate tasks in CI */
   eslint: { ignoreDuringBuilds: true },
   typescript: { ignoreBuildErrors: true },
+
+  webpack: (config, { isServer }) => {
+    if (isServer) {
+      config.plugins = [...config.plugins, new PrismaPlugin()];
+    }
+
+    return config;
+  },
 };
 
 export default config;
